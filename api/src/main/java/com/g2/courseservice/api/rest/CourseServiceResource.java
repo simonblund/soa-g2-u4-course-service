d57d26769d0362cfe255afc6a1deeca8ff5d80bd
Simon Blomsterlund <simon@simonblund.com>
2021-01-10 01:13:51 +0200

    Add importable resource

---
+++ api/src/main/java/com/g2/courseservice/api/rest/CourseServiceResource.java
@@ -0,0 +1,29 @@
+package com.g2.courseservice.api.rest;
+
+import com.g2.courseservice.api.rest.course.CourseRequest;
+import com.g2.courseservice.api.rest.course.CourseResponse;
+import com.g2.courseservice.api.rest.courseinstance.CourseOccasionResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public interface CourseServiceResource {
+    @GetMapping(UrlPaths.COURSE_RESOURCE)
+    ResponseEntity<List<CourseResponse>> getAllCourses();
+
+    @GetMapping(UrlPaths.GET_COURSE)
+    ResponseEntity<CourseResponse> findOneCourse(@PathVariable String courseCode);
+
+    @GetMapping(UrlPaths.GET_COURSE_INSTANCE)
+    ResponseEntity<CourseOccasionResponse> findOneCourseOccasion(@PathVariable long courseOccasionId);
+
+    @GetMapping(UrlPaths.COURSE_INSTANCES)
+    ResponseEntity<List<CourseOccasionResponse>> findCourseOccasionsByCourseCode(
+            @RequestParam(value = "course_code", required = false) String courseCode
+    );
+
+    @PostMapping(UrlPaths.COURSE_RESOURCE)
+    ResponseEntity<CourseResponse> createCourse(@RequestBody CourseRequest courseRequest);
+
+}
